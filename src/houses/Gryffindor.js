6f5063674fdbf7e625830a8cdb5a282406b66349
Jesse <jessyvee1@gmail.com>
2023-04-21 14:07:12 +0300

    asignment done

--- src/houses/Gryffindor.js
+++
@@ -1,9 +0,0 @@
-export const colors = "Scarlet and Gold";
-
-function values() {
-  console.log("Courage, Bravery, Nerve and Chivalry");
-}
-
-export function gryffMascot() {
-  console.log("The Lion");
-}
